bd31fa669bd2ec343eef1572772fe8584168eeac
Server <q3dm0@yahoo.com>
2017-12-26 09:16:50 +0300

    +

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'ubk-mebel');
+define('DB_NAME', 'mkvadrat_ubk');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'root');
+define('DB_USER', 'mkvadrat_ubk');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', '1234');
+define('DB_PASSWORD', 'k6CiUz4r');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
